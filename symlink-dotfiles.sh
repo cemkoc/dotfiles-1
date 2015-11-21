19647710225f3b2f07e72dcefc0820dde952c07c
Josh Habdas <jhabdas@gmail.com>
2015-11-21 00:59:45 -0600

    It didn't look fancy

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-fancy_echo() {
-  local fmt="$1"; shift
-  printf "\n$fmt\n" "$@"
-}
-
 dev="$HOME/Developer"
 dotfiles="$dev/dotfiles"
 
 if [[ -d "$dotfiles" ]]; then
-  fancy_echo "Symlinking dotfiles from $dotfiles"
+  echo "Symlinking dotfiles from $dotfiles"
 else
-  fancy_echo "$dotfiles does not exist"
+  echo "$dotfiles does not exist"
   exit 1
 fi
 
 link() {
   from="$1"
   to="$2"
-  fancy_echo "Linking '$from' to '$to'"
+  echo "Linking '$from' to '$to'"
   rm -f "$to"
   ln -s "$from" "$to"
 }
